b1a2cf1b54e67c10573e5bd874a7a08fc7c63c79
hammertime1070 <mbkeith614@gmail.com>
2023-10-10 15:52:20 -0400

    Add player controller

--- rust-city/src/player.rs
+++ rust-city/src/player.rs
@@ -20,18 +20,32 @@ pub enum Direction {
 pub fn players_move(
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<(
-        &PlayerNo,
         &mut Velocity,
         &mut Direction,
     )>,
 ) {
-    for (player_no, mut velocity, mut direction) in &mut query {
-        if player_no.0 == 1 && keyboard_input.any_just_released([KeyCode::W, KeyCode::A, KeyCode::S, KeyCode::D]) {
+    for (mut velocity, mut direction) in &mut query {
+        if keyboard_input.any_just_released([KeyCode::W, KeyCode::A, KeyCode::S, KeyCode::D]) {
             velocity.linvel = Vec2::ZERO;
             continue;
         }
-        if player_no.0 == 2 && keyboard_input.any_just_released([KeyCode::Up, KeyCode::Left, KeyCode::Down, KeyCode::Right]) {
-            velocity.linvel = Vec2::Zero;
+        if (keyboard_input.pressed(KeyCode::W)) {
+            velocity.linvel = Vec2::new(0.0, PLAYER_SPEED);
+            *direction = ::Up;
+        }
+        else if (keyboard_input.pressed(KeyCode::A)) {
+            velocity.linvel = Vec2::new(0.0, PLAYER_SPEED);
+            *direction = ::Left;
+        }
+        else if (keyboard_input.pressed(KeyCode::S)) {
+            velocity.linvel = Vec2::new(0.0, PLAYER_SPEED);
+            *direction = ::Down;
+        }
+        else if (keyboard_input.pressed(KeyCode::D)) {
+            velocity.linvel = Vec2::new(0.0, PLAYER_SPEED);
+            *direction = ::Right;
+        }
+        else {
             continue;
         }
     }
